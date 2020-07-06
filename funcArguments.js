9937e581dbf4466cd4e7a32805ea1c397c90149e
Eduardo Bonate <eduardobonate@hotmail.com>
2020-07-06 15:24:58 -0300

    Aula 2
    Incluido outros exemplos.

--- funcArguments.js
+++ funcArguments.js
@@ -1,9 +1,20 @@
-
-
-
 //Ao tentar multiplicar um numero por um undefined, o JS retorna um NAN(Not a number)
-function multiply(a, b) {
+/* function multiply(a, b = a) {
+    //Exemplo b = 1 ou b = a
     //Antigamente: Usava o operador logico Ou(||) - Exemplo b = b || 1 
+    //Exemplo: b = typeof b === 'undefined' ? 1 : b
     return a * b
 }
-console.log(multiply(5))
+console.log(multiply(5)) */
+
+//lazy evaluation
+
+function randomNumber() {
+    console.log('Generating a random number')
+    return Math.random() * 10;
+}
+
+function multiply(a, b = a){
+    return a + b
+}
+console.log(randomNumber())
