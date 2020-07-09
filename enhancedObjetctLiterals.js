3361ca8c304d320bc74415ed57f2e64854c73377
Eduardo Bonate <eduardobonate@hotmail.com>
2020-07-09 14:21:50 -0300

    Exemplos
    Incluido exemplos.

---
+++ enhancedObjetctLiterals.js
@@ -0,0 +1,45 @@
+//Atribuido propriedade ao objeto
+/* var obj = {
+    prop1: 'Digital Innovation One'
+}
+ */
+
+//Or
+/* var prop1 = 'Digital Innovation One'
+
+var obj2 = {
+    prop2: prop1
+}
+console.log(obj2) */
+
+//ECMA 6
+/* var obj3 = {
+    prop1
+}
+console.log(obj3) */
+
+//Short-handed
+/* function method1() {
+    console.log('method called')
+}
+
+var obj4 = {
+    method1
+}
+obj4.method1() */
+
+
+ //
+
+/* var obj = {
+
+    /* sum: function sum(a, b) {
+        return a + b
+    } */
+    //or - 
+    /* sum(a, b){
+        return a + b
+    } */
+}
+
+console.log(obj.sum(1, 5)) */
