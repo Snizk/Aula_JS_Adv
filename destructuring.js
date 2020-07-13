0dd614a81d7c453593dce9bb4997e2760f9cb851
Eduardo Bonate <eduardobonate@hotmail.com>
2020-07-13 10:37:45 -0300

    Inclusao
    Exemplos

---
+++ destructuring.js
@@ -0,0 +1,11 @@
+var arr = ['Apple', 'Banana', 'Orange']
+
+var apple = arr[0]
+var banana = arr[1]
+var orange = arr[2]
+
+//Destructuring Assigment
+
+var [apple2, banana2, orange2] = ['Apple', 'Banana', 'Orange']
+
+console.log(apple, apple2)
